cb96b61d34cc4a1716e22c289a97456a04552893
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-25 16:59:44 -0300

    chore: add `Bulma CSS`

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -4,6 +4,8 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Ci Comentarios</title>
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.1/css/bulma.min.css">
 </head>
 <body>
 	<?= $this->renderSection('content') ?>
